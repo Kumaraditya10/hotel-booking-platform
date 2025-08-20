bdaec5a2b7a409efde9d930417e5a2ad59e6c4cb
Aditya <aditya10112005@gmail.com>
2025-08-20 12:31:26 +0530

    initial commit

---
+++ server/controllers/userController.js
@@ -0,0 +1,13 @@
+
+
+//GET /api/user
+
+export const getUserData = async (req, res)=>{
+    try {
+        const role = req.user.role;
+        const recentSearchedCities = req.user.user.recentSearchedCities;
+        res.json({sucess: true, role, recentSearchedCities})
+    } catch (error) {
+        res.json({sucess: false, message: error.message})
+    }
+}
